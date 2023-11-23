8914a931245d9eb5b38fa6ee042e6b09258e4175
saeed <saeed00014@gmail.com>
2023-11-23 10:53:33 +0330

    first commit

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auths.js");
-const userRoutes = require("./routes/users.js");
-const postRoutes = require("./routes/posts.js");
-const commentRoutes = require("./routes/comments.js");
-const likeRoutes = require("./routes/likes.js");
-const messagesRoute = require("./routes/messages.js");
-const friendRuotes = require("./routes/friends.js");
-const productRuotes = require("./routes/product.js");
+// const userRoutes = require("./routes/users.js");
+// const postRoutes = require("./routes/posts.js");
+// const commentRoutes = require("./routes/comments.js");
+// const likeRoutes = require("./routes/likes.js");
+// const messagesRoute = require("./routes/messages.js");
+// const friendRuotes = require("./routes/friends.js");
+// const productRuotes = require("./routes/product.js");
 
 const app = express();
 
@@ -29,37 +29,37 @@ app.use(
 );
 app.use(cookieParser());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './src/assets/avatar')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
-})
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './src/assets/avatar')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname)
+//   }
+// })
 
-const upload = multer({ storage: storage })
+// const upload = multer({ storage: storage })
 
-app.post('/i', upload.single('image'), (req, res) => {
-  res.send(req.file.filename)
-});
+// app.post('/i', upload.single('image'), (req, res) => {
+//   res.send(req.file.filename)
+// });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/likes", likeRoutes);
-app.use("/api/messages", messagesRoute);
-app.use("/api/friends", friendRuotes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/likes", likeRoutes);
+// app.use("/api/messages", messagesRoute);
+// app.use("/api/friends", friendRuotes);
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    writeConcern: { w: 'majority' },
-  }
-);
+// mongoose.connect(
+//   process.env.MONGO_URL,
+//   {
+//     writeConcern: { w: 'majority' },
+//   }
+// );
   
-app.use('/products', productRuotes);
+// app.use('/products', productRuotes);
 
 const port = 7000;
 
